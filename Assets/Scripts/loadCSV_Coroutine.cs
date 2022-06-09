85b37d11862ba485fce325061d51a2c94ee99863
Ben <benodru@gmail.com>
2022-06-09 13:37:50 +0300

    added comments

--- Assets/Scripts/loadCSV_Coroutine.cs
+++ Assets/Scripts/loadCSV_Coroutine.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class loadCSV_Coroutine : MonoBehaviour
 {
     //make a list of sensors
+    //TODO: fix to dictionary
     //Dictionary<string, Sensors> sensors = new Dictionary<string, Sensors>(); 
     List<Sensors> sensors = new List<Sensors>(); 
  
@@ -37,17 +38,12 @@ public class loadCSV_Coroutine : MonoBehaviour
 
             
             //check if name is valid and not empty
+            //bug was in prevous code where if(row[i] ...) this is incorrect
+            //correct code fixed below
             if(row[1] != "") //if the row isn't empty then do the loop
             {
                 //initiate the Sensors class
                 Sensors s = new Sensors();
-                //casting a string to an int with
-                //int.TryParse(waht to parse, out where to put it in)
-                // int.TryParse(row[0], out s.id);
-                // s.sensorName = row[1];
-                // s.data = row[2];
-                // int.TryParse(row[3], out s.time);
-                // int.TryParse(row[4], out s.date);
                 s.SensorID = row[0];
                 s.Type = row[1];
                 s.Location = row[2];
